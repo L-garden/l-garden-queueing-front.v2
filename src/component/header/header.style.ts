01db6a89d1e3fc3b6f7698de49cead2bf95a886a
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-06-13 12:53:46 +0900

    헤더 font-family 명시 (deploy 0.2.2)

--- src/component/header/header.style.ts
+++ src/component/header/header.style.ts
@@ -5,4 +5,12 @@ export const HeaderSection = styled.div`
     flex-direction: row;
     justify-content: space-between;
     height: 60px;
+
+    @font-face {
+        font-family: 'Cafe24Ohsquare';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/Cafe24Ohsquare.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
+    font-family: "Cafe24Ohsquare";
 `
