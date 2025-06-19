413b5eeae84822a9a8e9e04df33f79b9ef4509ec
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-06-20 00:55:04 +0900

    사용자 정보 없을시 관리자 페이지 탈출 처리

--- src/component/header/title/Title.tsx
+++ src/component/header/title/Title.tsx
@@ -3,15 +3,24 @@ import {SteffNameDiv, TitleDiv, TitleSection} from "@/component/header/title/tit
 import {useTranslations} from "next-intl";
 import {useEffect, useState} from "react";
 import {AdminInfo} from "@/component/modalContent/adminSetting/AdminSetting.type";
+import {usePathname, useRouter} from "@/i18n/navigation";
 
 export default () => {
     const t = useTranslations("header");
     const [adminName, setAdminName] = useState<string | undefined>();
+    const pathName = usePathname();
+    const router = useRouter();
     useEffect(() => {
         const adminsInfoItem = sessionStorage.getItem("adminsInfo");
         if (adminsInfoItem) {
             const adminsInfo = JSON.parse(String(adminsInfoItem)) as AdminInfo;
-            setAdminName(adminsInfo.adminName);
+            if (adminsInfo.adminName && adminsInfo.adminName && adminsInfo.adminRole) {
+                setAdminName(adminsInfo.adminName);
+                return;
+            }
+        }
+        if (pathName === '/admin') {
+            router.replace("/queue");
         }
     }, []);
     return (
