e212b5b9a6b480d925b095a4d70a1909f3f7af92
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-06-20 00:55:44 +0900

    로그아웃 기능 구현 (deploy 0.14.9)

--- src/component/modalContent/adminSetting/console/AdminSettingConsoleContent.tsx
+++ src/component/modalContent/adminSetting/console/AdminSettingConsoleContent.tsx
@@ -1,23 +1,29 @@
 import {useTranslations} from "next-intl";
 import {ModalButton, ModalSection} from "@/component/modalContent/styles/modalNormal.style";
-import {Link} from "@/i18n/navigation";
+import {useRouter} from "@/i18n/navigation";
 import {SetSettingStatusProp} from "@/component/modalContent/adminSetting/AdminSetting.type";
+import {deleteCookie} from "cookies-next";
 
 export default ({setSettingStatus}: SetSettingStatusProp) => {
     const t = useTranslations("modal.admin.setting.console");
+    const router = useRouter();
     const changePw = () => {
         setSettingStatus("changePw");
     }
     const adminChange = () => {
         setSettingStatus("adminChange");
     }
+    const logout = () => {
+        sessionStorage.removeItem('adminsInfo');
+        sessionStorage.removeItem('access');
+        deleteCookie("refresh");
+        router.replace('/queue');
+    }
     return (
         <ModalSection>
             <ModalButton onClick={changePw}>{t('changePassword')}</ModalButton>
             <ModalButton onClick={adminChange}>{t('adminChange')}</ModalButton>
-            <Link href="/queue">
-                <ModalButton>{t('logout')}</ModalButton>
-            </Link>
+            <ModalButton onClick={logout}>{t('logout')}</ModalButton>
         </ModalSection>
     )
 }
