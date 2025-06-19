5c506ada073c0634d9816da34d24497f412fdebc
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-06-20 01:06:45 +0900

    직원 관리 접근 권한 제한 구현

--- src/component/modalContent/adminSetting/console/AdminSettingConsoleContent.tsx
+++ src/component/modalContent/adminSetting/console/AdminSettingConsoleContent.tsx
@@ -1,23 +1,37 @@
 import {useTranslations} from "next-intl";
 import {ModalButton, ModalSection} from "@/component/modalContent/styles/modalNormal.style";
 import {useRouter} from "@/i18n/navigation";
-import {SetSettingStatusProp} from "@/component/modalContent/adminSetting/AdminSetting.type";
+import {AdminInfo, SetSettingStatusProp} from "@/component/modalContent/adminSetting/AdminSetting.type";
 import {deleteCookie} from "cookies-next";
+import {useCallback} from "react";
 
 export default ({setSettingStatus}: SetSettingStatusProp) => {
     const t = useTranslations("modal.admin.setting.console");
     const router = useRouter();
-    const changePw = () => {
-        setSettingStatus("changePw");
-    }
-    const adminChange = () => {
-        setSettingStatus("adminChange");
-    }
-    const logout = () => {
+    const kickUser = useCallback(() => {
         sessionStorage.removeItem('adminsInfo');
         sessionStorage.removeItem('access');
         deleteCookie("refresh");
         router.replace('/queue');
+    }, []);
+    const changePw = () => {
+        setSettingStatus("changePw");
+    }
+    const adminChange = () => {
+        const adminsInfoString = sessionStorage.getItem('adminsInfo');
+        if (!adminsInfoString) {
+            kickUser();
+            return;
+        }
+        const adminsInfo = JSON.parse(adminsInfoString) as AdminInfo;
+        if (!adminsInfo.adminRole || (adminsInfo.adminRole !== 'CEO' && adminsInfo.adminRole !== 'DEVELOPER')) {
+            setSettingStatus("adminChangeDenied");
+            return;
+        }
+        setSettingStatus("adminChange");
+    }
+    const logout = () => {
+        kickUser();
     }
     return (
         <ModalSection>
